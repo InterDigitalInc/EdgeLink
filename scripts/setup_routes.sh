11ba5ef3d2a748f2dc8bfbf919549db48edc6d3e
Simon Pastor <simon.pastor@interdigital.com>
2019-04-16 14:42:49 -0400

    commit

--- scripts/setup_routes.sh
+++ scripts/setup_routes.sh
@@ -3,6 +3,6 @@
 #route add -host <IP of GW node> dev <interface to reach the GW>
 #route add -net <IP subnet of NonGW nodes>/<NonGW network mask> dev <interface to reach the GW> gw <IP of the GW node>
 
-route add -host 192.168.200.10 dev eth0
-route add -net 192.168.150.0/24 dev eth0 gw 192.168.200.10
+sudo route add -host 192.168.200.10 dev eth0
+sudo route add -net 192.168.150.0/24 dev eth0 gw 192.168.200.10
 
