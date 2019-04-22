03f2211b8ef79f01d6222dd83e8f36f7f46a1a7f
pastorsx <49002255+pastorsx@users.noreply.github.com>
2019-04-22 09:55:18 -0400

    Update setup_java.sh

--- scripts/setup_java.sh
+++ scripts/setup_java.sh
@@ -3,6 +3,6 @@
 sudo apt-get install software-properties-common -y && \
 sudo add-apt-repository ppa:webupd8team/java -y && \
 sudo apt-get update && \
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections && \
-sudo apt-get install oracle-java8-installer oracle-java8-set-default -y
+sudo apt-get install default-jdk -y && \
+sudo apt-get install openjfx -y
 
