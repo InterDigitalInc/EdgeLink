a19614a5d8f606c9ef8cd9846a5841def2374cea
pastorsx <49002255+pastorsx@users.noreply.github.com>
2019-04-25 08:54:32 -0400

    Update setup_tomcatv8.sh

--- scripts/setup_tomcatv8.sh
+++ scripts/setup_tomcatv8.sh
@@ -8,7 +8,7 @@ BASEDIR=$(dirname "$SCRIPT")
 sudo groupadd tomcat
 sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
 cd /tmp
-wget http://apache.mirrors.ionfish.org/tomcat/tomcat-8/v8.5.39/bin/apache-tomcat-8.5.39.tar.gz
+wget http://apache.mirrors.ionfish.org/tomcat/tomcat-8/v8.5.40/bin/apache-tomcat-8.5.40.tar.gz
 sudo mkdir /opt/tomcat
 
 #installation of tomcat server
