9d974149b6ffa0e4f88ab979f57c12e51c0136a8
pastorsx <49002255+pastorsx@users.noreply.github.com>
2019-05-14 10:34:26 -0400

    Update setup_routes.sh

--- scripts/setup_routes.sh
+++ scripts/setup_routes.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+#  ~ =============================================================================
+#  ~
+#  ~                     Copyright (c) 2019
+#  ~                 InterDigital Communications, Inc.
+#  ~
+#  ~                      All rights reserved.
+#  ~
+#  ~  Licensed under the terms and conditions provided in InterDigital Labs Public
+#  ~  License v.1 (the “License”). You may not use this file except in compliance
+#  ~  with the License. Unless required by applicable law or agreed to in writing,
+#  ~  software distributed under the License is distributed on as “AS IS” BASIS,
+#  ~  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+#  ~  the License for the specific language governing permissions and limitations
+#  ~  under the License.
+#  ~
+#  ~ =============================================================================
+
 #route add -host <IP of GW node> dev <interface to reach the GW>
 #route add -net <IP subnet of NonGW nodes>/<NonGW network mask> dev <interface to reach the GW> gw <IP of the GW node>
 
